28f081148f3405859ceab4167f249fdd9cad8f53
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-24 13:38:36 +0300

    feat: add "getSumOfEvenNumbers" method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+        System.out.println();
     }
 }
--- src/com/it_academy/homework_1/Homework1.java
+++ src/com/it_academy/homework_1/Homework1.java
@@ -21,4 +21,33 @@ public class Homework1 {
         }
         return arraySum;
     }
+
+    /**
+     * Calculates the sum of all even Fibonacci numbers up to a definite index
+     * @param countOfNumbersInSequence the length of the Fibonacci numbers sequence
+     * @param amountIndex the number up to which the sum is calculated
+     * @return the sum of even numbers
+     */
+    public static int getSumOfEvenNumbers(int countOfNumbersInSequence, int amountIndex) {
+        int[] fibonacciNumbers = makeFibonacciSequence(countOfNumbersInSequence);
+        int amountOfNumbersFromSequence = 0;
+
+        for (int i = 0; i < amountIndex; i++) {
+            if (fibonacciNumbers[i] % 2 == 0) {
+                amountOfNumbersFromSequence += fibonacciNumbers[i];
+            }
+        }
+        return amountOfNumbersFromSequence;
+    }
+
+    private static int[] makeFibonacciSequence(int countOfNumbersInSequence) {
+        int[] fibonacciSequence = new int[countOfNumbersInSequence];
+
+        for (int i = 0; i < countOfNumbersInSequence; i++) {
+            fibonacciSequence[i] = i < 2
+                    ? i
+                    : fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+        }
+        return fibonacciSequence;
+    }
 }
