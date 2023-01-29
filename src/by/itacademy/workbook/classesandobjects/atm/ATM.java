6ed489d2bf24b5eb82f9e36771efba43c7c7e8e5
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-29 21:33:53 +0300

    feat: add "addMoney" method The method calculates the total amount of money in the ATM after adding banknotes.

--- src/by/itacademy/workbook/classesandobjects/atm/ATM.java
+++ src/by/itacademy/workbook/classesandobjects/atm/ATM.java
@@ -4,12 +4,35 @@ public class ATM {
     private int countOfBanknotes20;
     private int countOfBanknotes50;
     private int countOfBanknotes100;
+    private int totalAmountOfMoney;
 
 
     public ATM(int countOfBanknotes20, int countOfBanknotes50, int countOfBanknotes100) {
         this.countOfBanknotes20 = countOfBanknotes20;
         this.countOfBanknotes50 = countOfBanknotes50;
         this.countOfBanknotes100 = countOfBanknotes100;
+
+        totalAmountOfMoney = countOfBanknotes20 * 20
+                + countOfBanknotes50 * 50
+                + countOfBanknotes100 * 100;
+    }
+
+    public int addMoney(int kindOfBanknote, int countOfBanknotes) {
+        totalAmountOfMoney += switch (kindOfBanknote) {
+            case 20, 50, 100 -> kindOfBanknote * countOfBanknotes;
+            default -> throw new IllegalStateException("Unexpected value: " + kindOfBanknote);
+        };
+        return totalAmountOfMoney;
+    }
+
+    @Override
+    public String toString() {
+        return "ATM{ " +
+                "countOfBanknotes20=" + countOfBanknotes20 +
+                ", countOfBanknotes50=" + countOfBanknotes50 +
+                ", countOfBanknotes100=" + countOfBanknotes100 +
+                ", totalAmountOfMoney=" + totalAmountOfMoney +
+                 " }";
     }
 
     public int getCountOfBanknotes20() {
@@ -36,4 +59,11 @@ public class ATM {
         this.countOfBanknotes100 = countOfBanknotes100;
     }
 
+    public int getTotalAmountOfMoney() {
+        return totalAmountOfMoney;
+    }
+
+    public void setTotalAmountOfMoney(int totalAmountOfMoney) {
+        this.totalAmountOfMoney = totalAmountOfMoney;
+    }
 }
