304b2a7b81381fd0b6ded49ddc86c76bfcfc1548
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-07 11:32:27 +0300

    pref: add "getLongestRowOfNumbers" method

--- src/by/itacademy/additionalTasks/regex/RegEx.java
+++ src/by/itacademy/additionalTasks/regex/RegEx.java
@@ -1,5 +1,8 @@
 package by.itacademy.additionalTasks.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegEx {
     public static String replaceABC(String str) {
         if (str.startsWith("abc")) {
@@ -11,5 +14,22 @@ public class RegEx {
 
     public static String removeSpaces(String str) {
         return str.replaceAll("\\s+", " ").trim();
+    }
+
+    public static String getLongestRowOfNumbers(String str) {
+        Pattern pattern  = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(str);
+        String longestRowStr = "";
+        int longestRow = Integer.MIN_VALUE;
+        int temp;
+
+        while (matcher.find()) {
+            temp = matcher.group().length();
+            if (temp > longestRow) {
+                longestRow = temp;
+                longestRowStr = matcher.group();
+            }
+        }
+        return "Longest row: " + longestRow + " -> " + longestRowStr;
     }
 }
--- src/by/itacademy/additionalTasks/regex/RegExCheck.java
+++ src/by/itacademy/additionalTasks/regex/RegExCheck.java
@@ -4,5 +4,6 @@ public class RegExCheck {
     public static void main(String[] args) {
         System.out.println(RegEx.replaceABC("abcHello") + "\n");
         System.out.println(RegEx.removeSpaces("    fdfd    dsaf   fd    df  "));
+        System.out.println(RegEx.getLongestRowOfNumbers("dgffd12fgf1256456714563fgdg123456dfgdfg12345"));
     }
 }
