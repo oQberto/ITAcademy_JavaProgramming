919cf8f9066a07793ccbe08cc0c1c7bee9584e1f
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-07 11:03:27 +0300

    pref: add "replaceABC" method

---
+++ src/by/itacademy/additionalTasks/regex/RegEx.java
@@ -0,0 +1,11 @@
+package by.itacademy.additionalTasks.regex;
+
+public class RegEx {
+    public static String replaceABC(String str) {
+        if (str.startsWith("abc")) {
+            return str.replace("abc", "www");
+        } else {
+            return str + "zzz";
+        }
+    }
+}
---
+++ src/by/itacademy/additionalTasks/regex/RegExCheck.java
@@ -0,0 +1,7 @@
+package by.itacademy.additionalTasks.regex;
+
+public class RegExCheck {
+    public static void main(String[] args) {
+        System.out.println(RegEx.replaceABC("abcHello"));
+    }
+}
