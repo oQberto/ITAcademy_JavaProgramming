ee5dfe692aa19fc7f7c9046439d4bc2bee420c9d
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-09 17:40:53 +0300

    feat: fill "FlowerColor", "FlowerType" enums

--- src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/FlowerColor.java
+++ src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/FlowerColor.java
@@ -1,4 +1,21 @@
 package by.itacademy.javaprogrammingmethodes.consoleApp.flowerShop;
 
 public enum FlowerColor {
+    WHITE("White"),
+    YELLOW("Yellow"),
+    ORANGE("Orange"),
+    RED("Red"),
+    PURPLE("Purple"),
+    BLUE("Blue"),
+    GREEN("Green"),
+    BLACK("Black");
+    private final String flowerColor;
+
+    FlowerColor(String flowerColor) {
+        this.flowerColor = flowerColor;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
 }
--- src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/FlowerType.java
+++ src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/FlowerType.java
@@ -1,4 +1,20 @@
 package by.itacademy.javaprogrammingmethodes.consoleApp.flowerShop;
 
 public enum FlowerType {
+    CHAMOMILE("Chamomile"),
+    ROSE("Rose"),
+    TULIP("Tulip"),
+    LILY("Lily"),
+    BEGONIA("Begonia"),
+    PEONY("Peony"),
+    ORCHID("Orchid");
+    private final String flowerType;
+
+    FlowerType(String flowerType) {
+        this.flowerType = flowerType;
+    }
+
+    public String getFlowerType() {
+        return flowerType;
+    }
 }
