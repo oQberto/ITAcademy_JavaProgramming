3cc57311e89881b115fe4a84ead3b9cdc93ba58b
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-10 16:57:08 +0300

    feat: add more types of bouquets

--- src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/bouquets/BouquetType.java
+++ src/by/itacademy/javaprogrammingmethodes/consoleApp/flowerShop/bouquets/BouquetType.java
@@ -1,18 +1,26 @@
 package by.itacademy.javaprogrammingmethodes.consoleApp.flowerShop.bouquets;
 
 public enum BouquetType {
-    BASKET(5, "Basket"),
-    PAPER(2, "Paper");
+    SMALL_BASKET(5, 5, "Small basket"),
+    MIDDLE_BASKET(5, 10, "Middle basket"),
+    LARGE_BASKET(5, 15, "Large basket"),
+    PAPER(2, 20, "Paper");
     private final int price;
+    private final int bouquetCapacity;
     private final String bouquetType;
 
-    BouquetType(int price, String bouquetType) {
+    BouquetType(int price, int bouquetCapacity, String bouquetType) {
         this.price = price;
+        this.bouquetCapacity = bouquetCapacity;
         this.bouquetType = bouquetType;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    public int getBouquetCapacity() {
+        return bouquetCapacity;
     }
 
     public String getBouquetType() {
