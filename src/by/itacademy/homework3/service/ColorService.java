3dc4b3663d56dd4244969c517592e39643882229
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-15 16:59:18 +0300

    refactor: remove space

--- src/by/itacademy/homework3/service/ColorService.java
+++ src/by/itacademy/homework3/service/ColorService.java
@@ -5,7 +5,7 @@ import by.itacademy.homework3.car.CarColor;
 
 public class ColorService implements Changeable {
     @Override
-    public  void change(Car car, String carColor) {
+    public void change(Car car, String carColor) {
         for (CarColor color : CarColor.values()) {
             if (carColor.equals(color.getColorName())) {
                 car.setCarColor(color);
