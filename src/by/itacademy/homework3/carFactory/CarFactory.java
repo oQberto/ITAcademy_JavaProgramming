002095bbc8da28e4d0ed535c46b532f426f74dd3
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-16 15:51:07 +0300

    feat: change lists initialization complete task3

--- src/by/itacademy/homework3/carFactory/CarFactory.java
+++ src/by/itacademy/homework3/carFactory/CarFactory.java
@@ -3,37 +3,21 @@ package by.itacademy.homework3.carFactory;
 import by.itacademy.homework3.car.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarFactory {
-    private final List<CarBrand> carBrandList = new ArrayList<>();
-    private final List<CarEngine> carEngineList = new ArrayList<>();
-    private final List<CarColor> carColorList = new ArrayList<>();
-    private final List<CarWheelSize> carWheelSizeList = new ArrayList<>();
-    private final FactoryStock factoryStock = new FactoryStock();
-
+    private final List<CarBrand> carBrands;
+    private final List<CarEngine> carEngines;
+    private final List<CarColor> carColors;
+    private final List<CarWheelSize> carWheelSizes;
+    private final FactoryStock factoryStock;
     {
-        carBrandList.add(CarBrand.AUDI);
-        carBrandList.add(CarBrand.KIA);
-        carBrandList.add(CarBrand.VOLVO);
-
-        carEngineList.add(CarEngine.B4204T26);
-        carEngineList.add(CarEngine.G4KM_MPI);
-        carEngineList.add(CarEngine.V6_TFSI);
-        carEngineList.add(CarEngine.V8_TFSI);
-        carEngineList.add(CarEngine.V6_TFSI);
-        carEngineList.add(CarEngine.T5);
-
-        carColorList.add(CarColor.BLUE);
-        carColorList.add(CarColor.BLUE);
-        carColorList.add(CarColor.GREEN);
-        carColorList.add(CarColor.RED);
-        carColorList.add(CarColor.WHITE);
-        carColorList.add(CarColor.YELLOW);
-
-        carWheelSizeList.add(CarWheelSize.SMALL);
-        carWheelSizeList.add(CarWheelSize.MIDDLE);
-        carWheelSizeList.add(CarWheelSize.LARGE);
+        carBrands = new ArrayList<>(Arrays.asList(CarBrand.values()));
+        carEngines = new ArrayList<>(Arrays.asList(CarEngine.values()));
+        carColors = new ArrayList<>(Arrays.asList(CarColor.values()));
+        carWheelSizes = new ArrayList<>(Arrays.asList(CarWheelSize.values()));
+        factoryStock = new FactoryStock();
     }
 
     public <T> void showWorkShopCatalogue(List<T> catalogue) {
@@ -43,39 +27,79 @@ public class CarFactory {
     }
 
     public Car createCar(Car clientOrder) {
-        return new Car(
-                clientOrder.getCarBrand(),
-                clientOrder.getCarColor(),
-                clientOrder.getCarEngine(),
-                clientOrder.getWheelSize());
+        if (checkCarInTheStock(clientOrder) == null) {
+            return clientOrder;
+        } else {
+            return replaceInappropriateOptions(chooseMoreSuitableCar(clientOrder), clientOrder);
+        }
     }
 
-    public List<CarBrand> getCarBrandList() {
-        return carBrandList;
-    }
-
-    public List<CarEngine> getCarEngineList() {
-        return carEngineList;
-    }
-
-    private Car chooseMoreSuitableCar() {
+    private Car checkCarInTheStock(Car testedCar) {
+        for (Car car : factoryStock.getCars()) {
+            if (testedCar.equals(car)) {
+                return car;
+            }
+        }
         return null;
     }
 
-    private void checkCarInTheStock() {
+    public Car chooseMoreSuitableCar(Car clientOrder) {
+        Car moreSuitableCar = null;
+        int count = 0;
+        int maxMatches = 0;
+        for (Car car : factoryStock.getCars()) {
+            if (car.getCarBrand().equals(clientOrder.getCarBrand())
+                    && car.getCarEngine().equals(clientOrder.getCarEngine())
+                    && car.getIssueYear() == clientOrder.getIssueYear()) {
+                count++;
 
+                if (car.getCarColor().equals(clientOrder.getCarColor())) {
+                    count++;
+                }
+                if (car.getWheelSize().equals(clientOrder.getWheelSize())) {
+                    count++;
+                }
+            }
+            if (count > maxMatches) {
+                moreSuitableCar = car;
+                maxMatches = count;
+            }
+            count = 0;
+        }
+        return moreSuitableCar;
     }
 
-    private void replaceInappropriateOptions() {
-
+    private Car replaceInappropriateOptions(Car moreSuitableCar,  Car clientOrder) {
+        if (moreSuitableCar == null) {
+            return null;
+        }
+        if (!(moreSuitableCar.getCarColor().equals(clientOrder.getCarColor()))) {
+            moreSuitableCar.setCarColor(clientOrder.getCarColor());
+        }
+        if (!(moreSuitableCar.getWheelSize().equals(clientOrder.getWheelSize()))) {
+            moreSuitableCar.setWheelSize(clientOrder.getWheelSize());
+        }
+        if (!(moreSuitableCar.getOptions() == null
+                || moreSuitableCar.getOptions().equals(clientOrder.getOptions()))) {
+            moreSuitableCar.setOptions(clientOrder.getOptions());
+        }
+        return moreSuitableCar;
     }
 
-    public List<CarColor> getCarColorList() {
-        return carColorList;
+    public List<CarBrand> getCarBrands() {
+        return carBrands;
     }
 
-    public List<CarWheelSize> getCarWheelSizeList() {
-        return carWheelSizeList;
+    public List<CarEngine> getCarEngines() {
+        return carEngines;
+    }
+
+    public List<CarColor> getCarColors() {
+        return carColors;
+    }
+
+    public List<CarWheelSize> getCarWheelSizes() {
+        return carWheelSizes;
     }
 
     public FactoryStock getFactoryStock() {
