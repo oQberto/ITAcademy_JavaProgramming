9a464a108b803e35f2070a7e3bbd8cd4216b836c
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-18 15:52:38 +0300

    feat: add HelicopterBrand field

--- src/by/itacademy/additionalTasks/inheritance/airline/aircraft/helicopter/Helicopter.java
+++ src/by/itacademy/additionalTasks/inheritance/airline/aircraft/helicopter/Helicopter.java
@@ -3,8 +3,26 @@ package by.itacademy.additionalTasks.inheritance.airline.aircraft.helicopter;
 import by.itacademy.additionalTasks.inheritance.airline.aircraft.Quadcopter;
 
 public class Helicopter extends Quadcopter {
+    private HelicopterBrand helicopterBrand;
 
-    public Helicopter(int flightRage, int flightHeight, int capacity, int loadCapacity) {
+    public Helicopter(int flightRage, int flightHeight, int capacity,
+                      int loadCapacity, HelicopterBrand helicopterBrand) {
         super(flightRage, flightHeight, capacity, loadCapacity);
+        this.helicopterBrand = helicopterBrand;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ". Helicopter{ " +
+                "helicopterBrand=" + helicopterBrand +
+                " }";
+    }
+
+    public HelicopterBrand getHelicopterBrand() {
+        return helicopterBrand;
+    }
+
+    public void setHelicopterBrand(HelicopterBrand helicopterBrand) {
+        this.helicopterBrand = helicopterBrand;
     }
 }
