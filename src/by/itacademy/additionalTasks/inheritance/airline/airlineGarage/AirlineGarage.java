9e2052015a68a2b94e9752adfedd6f39f2b22403
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-02-18 16:21:58 +0300

    feat: add aircrafts to AirlineGarage

--- src/by/itacademy/additionalTasks/inheritance/airline/airlineGarage/AirlineGarage.java
+++ src/by/itacademy/additionalTasks/inheritance/airline/airlineGarage/AirlineGarage.java
@@ -2,12 +2,29 @@ package by.itacademy.additionalTasks.inheritance.airline.airlineGarage;
 
 import by.itacademy.additionalTasks.inheritance.airline.aircraft.Aircraft;
 import by.itacademy.additionalTasks.inheritance.airline.aircraft.AircraftComparator;
+import by.itacademy.additionalTasks.inheritance.airline.aircraft.Quadcopter;
+import by.itacademy.additionalTasks.inheritance.airline.aircraft.airplane.Airplane;
+import by.itacademy.additionalTasks.inheritance.airline.aircraft.airplane.AirplaneBrand;
+import by.itacademy.additionalTasks.inheritance.airline.aircraft.helicopter.Helicopter;
+import by.itacademy.additionalTasks.inheritance.airline.aircraft.helicopter.HelicopterBrand;
 
 import java.util.TreeSet;
 
 public class AirlineGarage {
     private final AircraftComparator aircraftComparator = new AircraftComparator();
     private TreeSet<Aircraft> aircraftTreeSet = new TreeSet<>(aircraftComparator);
+
+    {
+        aircraftTreeSet.add(new Airplane(5000, 10500, 225, 45,
+                AirplaneBrand.AIRBUS));
+        aircraftTreeSet.add(new Airplane(4000, 12500, 300, 25,
+                AirplaneBrand.BOEING));
+        aircraftTreeSet.add(new Helicopter(5000, 10500, 225, 45,
+                HelicopterBrand.AIRBUS));
+        aircraftTreeSet.add(new Helicopter(5000, 10500, 225, 45,
+                HelicopterBrand.BELL));
+        aircraftTreeSet.add(new Quadcopter(45, 500, 0, 12));
+    }
 
     public void addAircraftToGarage(Aircraft aircraft) {
         aircraftTreeSet.add(aircraft);
