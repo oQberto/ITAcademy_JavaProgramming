564d783823e0e71081e78911711c93c950873ef8
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-03 19:43:56 +0300

    feat: add Messages for the game. "Game instructions".

---
+++ src/by/itacademy/game/Messages.java
@@ -0,0 +1,9 @@
+package by.itacademy.game;
+
+public interface Messages {
+    String START_MESSAGE = "Game started. Write a number from 1 to 100";
+    String NOT_NUMBER_MESSAGE = "Write valid number from 1 to 100.";
+    String SECRET_NUMBER_IS_MORE = "Secret number is more.";
+    String SECRET_NUMBER_IS_LESS = "Secret number is less.";
+    String WIN = "You win for %s turns";
+}
