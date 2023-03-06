f00e540bc1c19cdcc597d2ca75d9a0f2b1fe6f32
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-06 10:18:57 +0300

    feat: add task8-homework5

---
+++ src/by/itacademy/homework5/task8/Main.java
@@ -0,0 +1,10 @@
+package by.itacademy.homework5.task8;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader reader = new Reader();
+        reader.writeEqualsStrings();
+    }
+}
---
+++ src/by/itacademy/homework5/task8/Reader.java
@@ -0,0 +1,56 @@
+package by.itacademy.homework5.task8;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Reader {
+    private static final String INPUT_1_PATH = "D:\\Java Projects\\ITAcademy_JavaProgramming\\src\\by\\itacademy\\homework5\\task8\\INPUT_1";
+    private static final String INPUT_2_PATH = "D:\\Java Projects\\ITAcademy_JavaProgramming\\src\\by\\itacademy\\homework5\\task8\\INPUT_2";
+    private static final String OUTPUT_PATH = "D:\\Java Projects\\ITAcademy_JavaProgramming\\src\\by\\itacademy\\homework5\\task8\\OUTPUT";
+    private BufferedReader bf;
+    private final Map<String, Integer> map = new HashMap<>();
+
+    public void writeEqualsStrings() throws IOException {
+        fillMap();
+        compareFiles();
+        writeEqualsLinesToFile();
+    }
+
+    private void fillMap() throws IOException {
+        bf = new BufferedReader(new FileReader(INPUT_1_PATH));
+        String line = bf.readLine();
+
+        while (line != null) {
+            if (!line.equals("")) {
+                map.put(line, 0);
+            }
+            line = bf.readLine();
+        }
+        bf.close();
+    }
+
+    private void compareFiles() throws IOException {
+        bf = new BufferedReader(new FileReader(INPUT_2_PATH));
+        String line = bf.readLine();
+        while (line != null) {
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getKey().equals(line)) {
+                    entry.setValue(entry.getValue() + 1);
+                }
+            }
+            line = bf.readLine();
+        }
+        bf.close();
+    }
+
+    private void writeEqualsLinesToFile() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                bw.write(entry.getKey() + "\n");
+            }
+        }
+        bw.close();
+    }
+}
