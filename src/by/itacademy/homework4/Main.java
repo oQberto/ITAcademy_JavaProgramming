c22fd491ad781c33cb07792c0be64d21350d8216
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-10 16:44:21 +0300

    feat: create TruckFactory

--- src/by/itacademy/homework4/Main.java
+++ src/by/itacademy/homework4/Main.java
@@ -6,16 +6,29 @@ import by.itacademy.homework4.car.enums.CarEngine;
 import by.itacademy.homework4.car.enums.CarWheelSize;
 import by.itacademy.homework4.factory.CarFactory;
 import by.itacademy.homework4.factory.SpecialCarFactory;
+import by.itacademy.homework4.factory.TruckFactory;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CarFactory carFactory = new SpecialCarFactory();
+        CarFactory carFactory;
+        Scanner IN = new Scanner(System.in);
+        String input = IN.nextLine();
 
-        System.out.println(carFactory.getCarsInStock());
-        System.out.println(carFactory.createCar(2023, CarBrand.FORD, CarEngine.COMMON_RAIL, CarColor.BLACK,
-                CarWheelSize.SMALL, new ArrayList<>()));
-        System.out.println(carFactory.getCarsInStock());
+        if (input.equals("truck")) {
+            carFactory = new TruckFactory();
+            System.out.println(carFactory.getCarsInStock());
+            System.out.println(carFactory.createCar(2023, CarBrand.VOLVO, CarEngine.COMMON_RAIL, CarColor.RED,
+                    CarWheelSize.SMALL, new ArrayList<>()));
+            System.out.println(carFactory.getCarsInStock());
+        } else if (input.equals("sp")) {
+            carFactory = new SpecialCarFactory();
+            System.out.println(carFactory.getCarsInStock());
+            System.out.println(carFactory.createCar(2023, CarBrand.FORD, CarEngine.COMMON_RAIL, CarColor.WHITE,
+                    CarWheelSize.SMALL, new ArrayList<>()));
+            System.out.println(carFactory.getCarsInStock());
+        }
     }
 }
--- src/by/itacademy/homework4/factory/TruckFactory.java
+++ src/by/itacademy/homework4/factory/TruckFactory.java
@@ -2,23 +2,72 @@ package by.itacademy.homework4.factory;
 
 import by.itacademy.homework4.car.Car;
 import by.itacademy.homework4.car.Truck;
+import by.itacademy.homework4.car.enums.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TruckFactory extends CarFactory {
+    private List<Truck> trucksInStock;
+    private final FactoryStock<Truck> truckFactoryStock;
 
-    @Override
-    public Car replaceInappropriateOptions() {
-        return null;
+    public TruckFactory() {
+        fillStock();
+        this.truckFactoryStock = new FactoryStock<>(trucksInStock);
     }
 
     @Override
-    public List<Car> getCarsInStock() {
-        return null;
+    public Car createCar(int issueYear,
+                         CarBrand carBrand,
+                         CarEngine carEngine,
+                         CarColor carColor,
+                         CarWheelSize carWheelSize,
+                         List<Options> options) {
+        if (truckFactoryStock.checkCar(issueYear, carBrand, carEngine, carColor, carWheelSize, options) != null) {
+            return truckFactoryStock.getClientCar();
+        } else if (replaceInappropriateOptions(
+                truckFactoryStock.chooseMoreSuitableCar(
+                        issueYear, carBrand, carEngine, carColor, carWheelSize, options),
+                issueYear, carBrand, carEngine, carColor, carWheelSize, options) != null) {
+            return replaceInappropriateOptions(truckFactoryStock.getMoreSuitableCar(), issueYear,
+                    carBrand, carEngine, carColor, carWheelSize, options);
+        }
+        return new Truck(issueYear, carBrand, carEngine, carColor, carWheelSize, options);
     }
 
     @Override
-    public Car createCar() {
-        return new Truck();
+    public Car replaceInappropriateOptions(Car truck, int issueYear,
+                                                        CarBrand carBrand,
+                                                        CarEngine carEngine,
+                                                        CarColor carColor,
+                                                        CarWheelSize carWheelSize,
+                                                        List<Options> options) {
+        if (truck == null) return null;
+        if (!(truck.getCarColor().equals(carColor))) {
+            truck.setCarColor(carColor);
+        }
+        if (!(truck.getWheelSize().equals(carWheelSize))) {
+            truck.setWheelSize(carWheelSize);
+        }
+        if (truck.getOptions().size() == 0
+                || truck.getOptions().equals(options)) {
+            truck.setOptions(options);
+        }
+        return truck;
+    }
+
+
+    @Override
+    public void fillStock() {
+        trucksInStock = new ArrayList<>();
+        trucksInStock.add(new Truck(2023, CarBrand.VOLVO, CarEngine.COMMON_RAIL, CarColor.RED,
+                CarWheelSize.LARGE, null));
+        trucksInStock.add(new Truck(2023, CarBrand.VOLVO, CarEngine.COMMON_RAIL, CarColor.WHITE,
+                CarWheelSize.SMALL, null));
+    }
+
+    @Override
+    public List<Truck> getCarsInStock() {
+        return trucksInStock;
     }
 }
