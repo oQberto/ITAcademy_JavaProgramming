1bced2749966d2332c61ac90e09fba143a902763
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-10 16:25:02 +0300

    style: change Truck's constructor order

--- src/by/itacademy/homework4/car/Truck.java
+++ src/by/itacademy/homework4/car/Truck.java
@@ -5,7 +5,12 @@ import by.itacademy.homework4.car.enums.*;
 import java.util.List;
 
 public class Truck extends Car {
-    public Truck(int issueYear, CarBrand carBrand, CarEngine carEngine, CarColor carColor, CarWheelSize carWheelSize, List<Options> options) {
+    public Truck(int issueYear,
+                 CarBrand carBrand,
+                 CarEngine carEngine,
+                 CarColor carColor,
+                 CarWheelSize carWheelSize,
+                 List<Options> options) {
         super(issueYear, carBrand, carEngine, carColor, carWheelSize, options);
     }
 
