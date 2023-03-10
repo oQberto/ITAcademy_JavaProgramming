0238d3a01451bd037f6d533af7efc2879cb217ce
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-10 11:05:21 +0300

    feat: create FactoryStock

---
+++ src/by/itacademy/homework4/factory/FactoryStock.java
@@ -0,0 +1,66 @@
+package by.itacademy.homework4.factory;
+
+import by.itacademy.homework4.car.Car;
+import by.itacademy.homework4.car.enums.*;
+
+import java.util.List;
+
+public class FactoryStock<T extends Car> {
+    private final List<T> carsInStock;
+    private T clientCar = null;
+    private T moreSuitableCar = null;
+
+    public FactoryStock(List<T> carsInStock) {
+        this.carsInStock = carsInStock;
+    }
+
+    public T checkCar(int issueYear, CarBrand carBrand, CarEngine carEngine,
+                      CarColor carColor, CarWheelSize carWheelSize, List<Options> optionsList) {
+        carsInStock.forEach(carFromStock -> {
+            if (carFromStock.getIssueYear() == issueYear &&
+                    carFromStock.getCarBrand() == carBrand &&
+                    carFromStock.getCarEngine() == carEngine &&
+                    carFromStock.getCarColor() == carColor &&
+                    carFromStock.getWheelSize() == carWheelSize &&
+                    carFromStock.getOptions() == optionsList) {
+                clientCar = carFromStock;
+            }
+        });
+        carsInStock.remove(clientCar);
+        return clientCar;
+    }
+
+    public T chooseMoreSuitableCar(int issueYear, CarBrand carBrand, CarEngine carEngine,
+                                   CarColor carColor, CarWheelSize carWheelSize, List<Options> optionsList) {
+        carsInStock.forEach(car -> {
+            if (checkImmutableParams(car, issueYear, carBrand, carEngine)) {
+                moreSuitableCar = car;
+            }
+            if (checkImmutableParams(car, issueYear, carBrand, carEngine) &&
+                    checkChangeableParams(car, carColor, carWheelSize)) {
+                moreSuitableCar = car;
+            }
+        });
+        carsInStock.remove(moreSuitableCar);
+        return moreSuitableCar;
+    }
+
+    private boolean checkImmutableParams(T car, int issueYear, CarBrand carBrand, CarEngine carEngine) {
+        return car.getIssueYear() == issueYear &&
+                car.getCarBrand() == carBrand &&
+                car.getCarEngine() == carEngine;
+    }
+
+    private boolean checkChangeableParams(T car, CarColor carColor, CarWheelSize carWheelSize) {
+        if (car.getCarColor() == carColor && car.getWheelSize() == carWheelSize) {
+            return true;
+        } else if (car.getWheelSize() == carWheelSize) {
+            return true;
+        }
+        return car.getCarColor() == carColor;
+    }
+
+    public void showFactoryStock() {
+        carsInStock.forEach(System.out::println);
+    }
+}
