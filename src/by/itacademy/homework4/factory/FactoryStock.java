047846ccea23005d8f10ce2a048ce206329a6ad9
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-23 14:12:51 +0300

    refactor: cover FactoryStock with checks for null

--- src/by/itacademy/homework4/factory/FactoryStock.java
+++ src/by/itacademy/homework4/factory/FactoryStock.java
@@ -2,9 +2,14 @@ package by.itacademy.homework4.factory;
 
 import by.itacademy.homework4.car.Car;
 import by.itacademy.homework4.order.Order;
-import static by.itacademy.homework4.validation.Validator.*;
 
+import static by.itacademy.homework4.validation.Message.NullMessages.*;
+
+import static java.util.Objects.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FactoryStock<CAR extends Car, ORDER extends Order> {
     private final List<CAR> carsInStock;
@@ -12,39 +17,44 @@ public class FactoryStock<CAR extends Car, ORDER extends Order> {
     private CAR moreSuitableCar = null;
 
     public FactoryStock(List<CAR> carsInStock) {
-        this.carsInStock = carsInStock;
+        this.carsInStock =
+                carsInStock == null
+                        ? new ArrayList<>()
+                        : carsInStock;
     }
 
     public CAR checkCar(ORDER order) {
-        isNullOrder(order);
+        requireNonNull(order, NULL_ORDER);
 
-        carsInStock.forEach(carFromStock -> {
-            if (order.compare(carFromStock)) {
-                clientCar = carFromStock;
-            }
-        });
+        carsInStock
+                .forEach(carFromStock -> {
+                    if (order.compare(carFromStock)) {
+                        clientCar = carFromStock;
+                    }
+                });
         carsInStock.remove(clientCar);
         return clientCar;
     }
 
     public CAR chooseMoreSuitableCar(ORDER order) {
-        isNullOrder(order);
+        requireNonNull(order, NULL_ORDER);
 
-        carsInStock.forEach(car -> {
-            if (checkImmutableParams(car, order)) {
-                moreSuitableCar = car;
-            }
-            if (checkImmutableParams(car, order) &&
-                    checkChangeableParams(car, order)) {
-                moreSuitableCar = car;
-            }
-        });
+        carsInStock
+                .forEach(car -> {
+                    if (checkImmutableParams(car, order)) {
+                        moreSuitableCar = car;
+                    }
+                    if (checkImmutableParams(car, order) &&
+                            checkChangeableParams(car, order)) {
+                        moreSuitableCar = car;
+                    }
+                });
         carsInStock.remove(moreSuitableCar);
         return moreSuitableCar;
     }
 
     private boolean checkImmutableParams(CAR car, ORDER order) {
-        isNullCar(car);
+        requireNonNull(car, NULL_CAR);
 
         return car.getIssueYear() == order.getIssueYear() &&
                 car.getCarBrand() == order.getBrand() &&
@@ -52,21 +62,32 @@ public class FactoryStock<CAR extends Car, ORDER extends Order> {
     }
 
     private boolean checkChangeableParams(CAR car, ORDER order) {
-        isNullCar(car);
+        requireNonNull(car, NULL_CAR);
 
-        return (checkGeneralChangeableParams(car, order)
-                && car.getUniqueParam() == order.getUniqueParam()) ||
+        return (checkGeneralChangeableParams(car, order) &&
+                Objects.equals(
+                        car.getUniqueParam(),
+                        order.getUniqueParam())) ||
                 checkGeneralChangeableParams(car, order);
     }
 
 
     private boolean checkGeneralChangeableParams(CAR car, ORDER order) {
-        if (car.getCarColor() == order.getColor() && car.getWheelSize() == order.getWheelSize()) {
+        if (Objects.equals(car.getCarColor(), order.getColor()) &&
+                Objects.equals(car.getWheelSize(), order.getWheelSize())
+        ) {
             return true;
-        } else if (car.getCarColor() == order.getColor()) {
+        } else if (Objects.equals(
+                car.getCarColor(),
+                order.getColor()
+        )) {
             return true;
         }
-        return car.getWheelSize() == order.getWheelSize();
+        return Objects
+                .equals(
+                        car.getWheelSize(),
+                        order.getWheelSize()
+                );
     }
 
     public void showFactoryStock() {
