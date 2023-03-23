a6ed4bbf4edf62d0010a9e9946c79b005a08dc9d
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-23 14:34:18 +0300

    refactor: remove unuseful methods

--- src/by/itacademy/homework4/validation/Validator.java
+++ src/by/itacademy/homework4/validation/Validator.java
@@ -2,44 +2,14 @@ package by.itacademy.homework4.validation;
 
 import by.itacademy.homework4.car.Car;
 import by.itacademy.homework4.car.markerinterfaces.*;
-import by.itacademy.homework4.order.Order;
 
 import java.util.Objects;
 
-import static by.itacademy.homework4.validation.Message.NullMessages.*;
 import static by.itacademy.homework4.validation.Message.NotExistMessage.*;
 import static java.util.Objects.*;
 
 public class Validator {
     private Validator() {
-    }
-
-    public static void isNullCar(Car car) {
-        isNullObject(car, NULL_CAR);
-    }
-
-    public static void isNullOrder(Order order) {
-        isNullObject(order, NULL_ORDER);
-    }
-
-    public static void isNullBrand(Brand brand) {
-        isNullObject(brand, NULL_BRAND);
-    }
-
-    public static void isNullColor(Color color) {
-        isNullObject(color, NULL_COLOR);
-    }
-
-    public static void isNullEngine(Engine engine) {
-        isNullObject(engine, NULL_ENGINE);
-    }
-
-    public static void isNullOption(Option options) {
-        isNullObject(options, NULL_OPTION);
-    }
-
-    public static void isNullWheelSize(WheelSize wheelSize) {
-        isNullObject(wheelSize, NULL_WHEEL_SIZE);
     }
 
     public static boolean isExistingColor(Car car, Color changeableColor) {
@@ -54,9 +24,5 @@ public class Validator {
             if (Objects.equals(changeableWheelSize, wheelSize)) return true;
         }
         throw new IllegalArgumentException(WHEEL_SIZE_NOT_EXIST);
-    }
-
-    private static void isNullObject(Object object, String message) {
-        if (isNull(object)) throw new NullPointerException(message);
     }
 }
