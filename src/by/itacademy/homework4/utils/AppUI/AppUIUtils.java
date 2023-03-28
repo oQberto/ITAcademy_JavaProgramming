94269677545e55aae86b5641ea05a5fd9c0fb30c
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-03-28 15:03:04 +0300

    feat: add AppUIUtils, add possibility choose car configuration

---
+++ src/by/itacademy/homework4/utils/AppUI/AppUIUtils.java
@@ -0,0 +1,48 @@
+package by.itacademy.homework4.utils.AppUI;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AppUIUtils {
+
+    private AppUIUtils(){}
+
+    public static Object chooseConfig(Object[] objects) {
+        Object object;
+        flag: while(true) {
+            String userBrand = userInput();
+            for (Object ob : objects) {
+                if (userBrand.equalsIgnoreCase(ob.toString())) {
+                    object = ob;
+                    break flag;
+                }
+            }
+            System.out.println("Invalid input!");
+        }
+        return object;
+    }
+
+    public static int chooseLoadCapacity() {
+        int loadCapacity;
+        while (true) {
+            int userLoadCapacity = Integer.parseInt(userInput());
+            if (userLoadCapacity > 0 && userLoadCapacity < 100) {
+                loadCapacity = userLoadCapacity;
+                break;
+            }
+            System.out.println("Invalid input!");
+        }
+        return loadCapacity;
+    }
+
+    public static String userInput() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
